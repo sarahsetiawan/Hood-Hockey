62a65f985c42c3634f4982f7227bef2474bd2338
Sam Lawrence <roaders79@gmail.com>
2025-02-25 12:42:22 -0500

    Connected front end and back end login/register page. Front end rerouting does not work.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import Form from "../components/Form"
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    return <Form route="/hood_hockey_app/token/" method="login" />
 }
 
 export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import Form from "../components/Form"
 
 function Register() {
-    return <Form route="/api/user/register/" method="register" />
+    return <Form route="/hood_hockey_app/user/register/" method="register" />
 }
 
 export default Register
